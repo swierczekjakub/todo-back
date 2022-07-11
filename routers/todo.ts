16fa3e02849b8144a46fd91d3d31f9a28934c5f5
Jakub Świerczek <swierczekjakub1@gmail.com>
2022-07-11 18:00:04 +0200

    Added all endpoints.

---
+++ routers/todo.ts
@@ -0,0 +1,34 @@
+import {Router} from "express";
+import {TodoRecord} from "../records/todo.record";
+import {TodoEntity} from "../types";
+import {ValidationError} from "../utils/errors";
+
+export const todoRouter = Router();
+
+todoRouter
+
+    .get('/', async (req, res) => {
+        const todos = await TodoRecord.getAll();
+        res.json(todos);
+    })
+    .post('/', async (req, res) => {
+        const newTodo = new TodoRecord(req.body as TodoEntity);
+        await newTodo.insert();
+        res.json(newTodo);
+    })
+    .patch('/:todoId', async (req, res) => {
+        const todo = await TodoRecord.getOne(req.params.todoId);
+        const {body} = req;
+        todo.name = body.name;
+        await todo.update();
+        res.json(todo);
+    })
+    .delete('/:todoId', async (req, res) => {
+        const todo = await TodoRecord.getOne(req.params.todoId);
+
+        if (!todo) {
+            throw new ValidationError('There is no such todo.');
+        }
+        await todo.delete();
+        res.end();
+    });
